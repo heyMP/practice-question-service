86bff36608863b5fad3cd7679045d4de196ccf2a
heymp <mgp140@gmail.com>
2020-10-14 16:13:06 -0400

    Added hasura action to docker swarm

--- hasura/actions/src/createQuestion.js
+++ hasura/actions/src/createQuestion.js
@@ -1,4 +1,7 @@
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
+const HASURA_GRAPHQL_ENDPOINT =
+  process.env.HASURA_GRAPHQL_ENDPOINT ||
+  "http://host.docker.internal:8080/v1/graphql";
 
 module.exports = function (app) {
   const HASURA_OPERATION = `
@@ -9,45 +12,42 @@ mutation createQuestion($question: String!, $answer: String!, $note: String = ""
 }
 `;
 
-// execute the parent operation in Hasura
-const execute = async (variables, headers) => {
-  const fetchResponse = await fetch(
-    "http://host.docker.internal:8080/v1/graphql",
-    {
+  // execute the parent operation in Hasura
+  const execute = async (variables, headers) => {
+    const fetchResponse = await fetch(HASURA_GRAPHQL_ENDPOINT, {
       headers,
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         query: HASURA_OPERATION,
-        variables
-      })
+        variables,
+      }),
+    });
+    const data = await fetchResponse.json();
+    console.log("DEBUG: ", data);
+    return data;
+  };
+
+  // Request Handler
+  app.post("/createQuestion", async (req, res) => {
+    // get request input
+    const { question, answer, note, tags } = req.body.input;
+
+    // run some business logic
+
+    // execute the Hasura operation
+    const { data, errors } = await execute(
+      { question, answer, note, tags },
+      req.body.session_variables
+    );
+
+    // if Hasura operation errors, then throw error
+    if (errors) {
+      return res.status(400).json(errors[0]);
     }
-  );
-  const data = await fetchResponse.json();
-  console.log('DEBUG: ', data);
-  return data;
+
+    // success
+    return res.json({
+      ...data.insert_questions_one,
+    });
+  });
 };
-  
-
-// Request Handler
-app.post('/createQuestion', async (req, res) => {
-
-  // get request input
-  const { question, answer, note, tags } = req.body.input;
-
-  // run some business logic
-
-  // execute the Hasura operation
-  const { data, errors } = await execute({ question, answer, note, tags }, req.body.session_variables);
-
-  // if Hasura operation errors, then throw error
-  if (errors) {
-    return res.status(400).json(errors[0])
-  }
-
-  // success
-  return res.json({
-    ...data.insert_questions_one
-  })
-
-});
-}
